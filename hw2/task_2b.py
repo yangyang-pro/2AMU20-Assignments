fb4d92c86b465aafa1be2630562e6a3249311636
iasonth95 <iasonth95@gmail.com>
2021-05-29 19:37:00 +0200

    hw2 add

---
+++ hw2/task_2b.py
@@ -0,0 +1,26 @@
+from scipy.sparse.csgraph import minimum_spanning_tree
+from scipy.sparse.csgraph import breadth_first_order
+from scipy.special import logsumexp
+import numpy as np
+import csv
+
+class BinaryCLT:
+    def __init__(self, data, root=None, alpha=0.01):
+        pass
+
+    def get_tree(self):
+        pass
+
+    def get_log_params(self):
+        pass
+
+    def log_prob(self, x, exhaustive=False):
+        pass
+
+    def sample(self, n_samples):
+        pass
+
+run = BinaryCLT(1)
+
+if __name__ == "__main__":
+    print(run)
