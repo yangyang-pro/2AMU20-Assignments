0a0bca238b06f572fb3b81b590cddc0a0f46d891
iasonth95 <iasonth95@gmail.com>
2021-05-29 21:17:01 +0200

    datasets load

--- hw2/task_2b.py
+++ hw2/task_2b.py
@@ -1,3 +1,4 @@
+from numpy.random import triangular
 from scipy.sparse.csgraph import minimum_spanning_tree
 from scipy.sparse.csgraph import breadth_first_order
 from scipy.special import logsumexp
@@ -6,7 +7,11 @@ import csv
 
 class BinaryCLT:
     def __init__(self, data, root=None, alpha=0.01):
-        pass
+        self.data = data
+        self.root = root
+        self.alpha = alpha 
+        Tcsr = minimum_spanning_tree(data)
+        Tcsr.toarray().astype(int)
 
     def get_tree(self):
         pass
@@ -20,7 +25,24 @@ class BinaryCLT:
     def sample(self, n_samples):
         pass
 
-run = BinaryCLT(1)
+
 
 if __name__ == "__main__":
-    print(run)
+
+    with open('baudio.train.data', 'r') as file:
+        reader = csv.reader(file, delimiter = ',')
+        train = np.array(list(reader)).astype(np.float)
+        
+
+    with open('baudio.test.data', 'r') as file:
+        reader = csv.reader(file, delimiter = ',')
+        test = np.array(list(reader)).astype(np.float)
+        
+    with open('baudio.valid.data', 'r') as file:
+        reader = csv.reader(file, delimiter = ',')
+        valid = np.array(list(reader)).astype(np.float)
+
+    #run = BinaryCLT(train)
+    print(train.shape)
+   
+  
