18794dbac5723d04dabaf43e03f03ebc28c03640
iasonth95 <iasonth95@gmail.com>
2021-06-15 19:13:39 +0200

    taska)

---
+++ hw3/datasets.py
@@ -0,0 +1,174 @@
+import errno
+import numpy as np
+import os
+import tempfile
+import urllib.request
+import shutil
+import gzip
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
+def mkdir_p(path):
+    """Linux mkdir -p"""
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
+def maybe_download(directory, url_base, filename):
+    filepath = os.path.join(directory, filename)
+    if os.path.isfile(filepath):
+        return False
+
+    if not os.path.isdir(directory):
+        mkdir_p(directory)
+
+    url = url_base + filename
+    _, zipped_filepath = tempfile.mkstemp(suffix='.gz')
+    print('Downloading {} to {}'.format(url, zipped_filepath))
+    urllib.request.urlretrieve(url, zipped_filepath)
+    print('{} Bytes'.format(os.path.getsize(zipped_filepath)))
+    print('Move to {}'.format(filepath))
+    shutil.move(zipped_filepath, filepath)
+    return True
+
+
+def maybe_download_mnist():
+    mnist_files = ['train-images-idx3-ubyte.gz',
+                   'train-labels-idx1-ubyte.gz',
+                   't10k-images-idx3-ubyte.gz',
+                   't10k-labels-idx1-ubyte.gz']
+
+    for file in mnist_files:
+        if not maybe_download('data/mnist', 'http://yann.lecun.com/exdb/mnist/', file):
+            continue
+        print('unzip data/mnist/{}'.format(file))
+        filepath = os.path.join('data/mnist/', file)
+        with gzip.open(filepath, 'rb') as f_in:
+            with open(filepath[0:-3], 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+
+
+def load_mnist():
+    """Load MNIST"""
+
+    maybe_download_mnist()
+
+    data_dir = 'data/mnist'
+
+    fd = open(os.path.join(data_dir, 'train-images-idx3-ubyte'))
+    loaded = np.fromfile(file=fd, dtype=np.uint8)
+    train_x = loaded[16:].reshape((60000, 784)).astype(np.float32)
+
+    fd = open(os.path.join(data_dir, 'train-labels-idx1-ubyte'))
+    loaded = np.fromfile(file=fd, dtype=np.uint8)
+    train_labels = loaded[8:].reshape((60000)).astype(np.float32)
+
+    fd = open(os.path.join(data_dir, 't10k-images-idx3-ubyte'))
+    loaded = np.fromfile(file=fd, dtype=np.uint8)
+    test_x = loaded[16:].reshape((10000, 784)).astype(np.float32)
+
+    fd = open(os.path.join(data_dir, 't10k-labels-idx1-ubyte'))
+    loaded = np.fromfile(file=fd, dtype=np.uint8)
+    test_labels = loaded[8:].reshape((10000)).astype(np.float32)
+
+    train_labels = np.asarray(train_labels)
+    test_labels = np.asarray(test_labels)
+
+    return train_x, train_labels, test_x, test_labels
+
+
+def maybe_download_fashion_mnist():
+    mnist_files = ['train-images-idx3-ubyte.gz',
+                   'train-labels-idx1-ubyte.gz',
+                   't10k-images-idx3-ubyte.gz',
+                   't10k-labels-idx1-ubyte.gz']
+
+    for file in mnist_files:
+        if not maybe_download('data/fashion-mnist', 'http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/', file):
+            continue
+        print('unzip data/fashion-mnist/{}'.format(file))
+        filepath = os.path.join('data/fashion-mnist/', file)
+        with gzip.open(filepath, 'rb') as f_in:
+            with open(filepath[0:-3], 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+
+
+def load_fashion_mnist():
+    """Load fashion-MNIST"""
+
+    maybe_download_fashion_mnist()
+
+    data_dir = 'data/fashion-mnist'
+
+    fd = open(os.path.join(data_dir, 'train-images-idx3-ubyte'))
+    loaded = np.fromfile(file=fd, dtype=np.uint8)
+    train_x = loaded[16:].reshape((60000, 784)).astype(np.float32)
+
+    fd = open(os.path.join(data_dir, 'train-labels-idx1-ubyte'))
+    loaded = np.fromfile(file=fd, dtype=np.uint8)
+    train_labels = loaded[8:].reshape(60000).astype(np.float32)
+
+    fd = open(os.path.join(data_dir, 't10k-images-idx3-ubyte'))
+    loaded = np.fromfile(file=fd, dtype=np.uint8)
+    test_x = loaded[16:].reshape((10000, 784)).astype(np.float32)
+
+    fd = open(os.path.join(data_dir, 't10k-labels-idx1-ubyte'))
+    loaded = np.fromfile(file=fd, dtype=np.uint8)
+    test_labels = loaded[8:].reshape(10000).astype(np.float32)
+
+    train_labels = np.asarray(train_labels)
+    test_labels = np.asarray(test_labels)
+
+    return train_x, train_labels, test_x, test_labels
+
+
+def show_some_images(x, sqrtN, fig_num=99):
+    plt.figure(fig_num)
+    plt.clf()
+    i = 1
+    for n1 in range(1, sqrtN+1):
+        for n2 in range(1, sqrtN + 1):
+            plt.subplot(sqrtN, sqrtN, i)
+            plt.imshow(np.reshape(x[i-1, :], (28, 28)), cmap='gray')
+            plt.axis('off')
+            i += 1
+
+
+def normalize_min_max(x, new_min=0.0, new_max=1.0):
+    min_val = np.min(x, 0, keepdims=True)
+    max_val = np.max(x, 0, keepdims=True)
+    x = (x - min_val) / (max_val-min_val + 1e-12)
+    x = x * (new_max - new_min) + new_min
+    return x
+
+
+def save_image_stack(samples, num_rows, num_columns, filename, margin=5, margin_gray_val=1., frame=0, frame_gray_val=0.0):
+    """Save image stack in a tiled image"""
+
+    # for gray scale, convert to rgb
+    if len(samples.shape) == 3:
+        samples = np.stack((samples,) * 3, -1)
+
+    height = samples.shape[1]
+    width = samples.shape[2]
+
+    samples -= samples.min()
+    samples /= samples.max()
+
+    img = margin_gray_val * np.ones((height*num_rows + (num_rows-1)*margin, width*num_columns + (num_columns-1)*margin, 3))
+    for h in range(num_rows):
+        for w in range(num_columns):
+            img[h*(height+margin):h*(height+margin)+height, w*(width+margin):w*(width+margin)+width, :] = samples[h*num_columns + w, :]
+
+    framed_img = frame_gray_val * np.ones((img.shape[0] + 2*frame, img.shape[1] + 2*frame, 3))
+    framed_img[frame:(frame+img.shape[0]), frame:(frame+img.shape[1]), :] = img
+
+    img = Image.fromarray(np.round(framed_img * 255.).astype(np.uint8))
+
+    img.save(filename)
